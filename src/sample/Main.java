dfb3c4e0f02610509c52ffe2cc2147ed0d769064
Jen Walker <jennie.walker@uconn.edu>
2019-03-21 14:07:22 -0400

    Main clean-up
    1. import javafx.animation.AnimationTimer; // package never used; unnecessary line
    
    More to come) I like the program and the code runs, but bugs exist in other classes

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
@@ -9,7 +8,6 @@ import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
-
 
 
 public class Main extends Application {
